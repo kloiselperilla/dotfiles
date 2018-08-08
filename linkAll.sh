93a561c8b957a3c12b15a07661ac1d89d1f0c725
KevinLoiselPerilla <kloiselperilla@gmail.com>
2018-08-08 09:38:19 -0500

    Neovim changes and less destructive link

--- linkAll.sh
+++ linkAll.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
+mkdir ~/.dotbackup
 echo Linking bash
-rm ~/.bashrc
-rm ~/.bash_logout
-rm ~/.bash_profile
-rm -r ~/.util
+mv ~/.bashrc ~/.dotbackup/bashrc
+mv ~/.bash_logout ~/.dotbackup/bash_logout
+mv ~/.bash_profile ~/.dotbackup/bash_profile
+mv ~/.util ~/.dotbackup/util
 ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -s ~/.dotfiles/bash/bash_logout ~/.bash_logout
 ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/bash/util ~/.util
 
 echo Linking tmux
-rm ~/.tmux.conf
+mv ~/.tmux.conf ~/.dotbackup/tmux.conf
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
-echo Linking vim
-rm -r ~/.vim
-rm ~/.vimrc
+echo Linking vim/neovim
+mv ~/.vim ~/.dotbackup/vim
+mv ~/.vimrc ~/.dotbackup/vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
+if [ ! -d ~/.config ]
+then
+    mkdir ~/.config
+fi
+if [ ! -d ~/.config/nvim ]
+then
+    mkdir ~/.config/nvim
+fi
+ln -s ~/.config/nvim/init.vim ~/.vimrc
+
 
 echo Linking git
-rm ~/.git-prompt.sh
-rm ~/.gitconfig
-rm ~/.gitignore_global
+mv ~/.git-prompt.sh ~/.dotbackup/git-prompt.sh
+mv ~/.gitconfig ~/.dotbackup/gitconfig
+mv ~/.gitignore_global ~/.dotbackup/gitignore_global
 ln -s ~/.dotfiles/git/git-prompt.sh ~/.git-prompt.sh
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
