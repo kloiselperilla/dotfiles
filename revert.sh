4d042ab998433b74f4d9047e5ea2cee872ab1e57
kloiselperilla <kloiselperilla@gmail.com>
2019-09-23 15:45:01 -0500

    Add revert script for backup dotfiles

---
+++ revert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mv ~/.dotbackup/bashrc ~/.bashrc
+mv ~/.dotbackup/bash_logout ~/.bash_logout
+mv ~/.dotbackup/bash_profile ~/.bash_profile
+mv ~/.dotbackup/util ~/.util
+mv ~/.dotbackup/tmux ~/.tmux
+mv ~/.dotbackup/tmux.conf ~/.tmux.conf
+mv ~/.dotbackup/vim ~/.vim
+mv ~/.dotbackup/vimrc ~/.vimrc
+mv ~/.dotbackup/git-prompt.sh ~/.git-prompt.sh
+mv ~/.dotbackup/gitconfig ~/.gitconfig
+mv ~/.dotbackup/gitignore_global ~/.gitignore_global
